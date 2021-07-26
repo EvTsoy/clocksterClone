62a8f347ffae5a4328278a98446887f0724e252e
EvTsoy <tsoy4807@gmail.com>
2021-07-26 12:34:45 +0500

    commit

--- database/migrations/2021_07_26_073345_create_states_table.php
+++ database/migrations/2021_07_26_073345_create_states_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserStateTable extends Migration
+class CreateStatesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateUserStateTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_state', function (Blueprint $table) {
+        Schema::create('states', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->string('state');
-            $table->string('flow');
+            $table->unsignedBigInteger('user_telegram_id')->default(0);
+            $table->unsignedBigInteger('message_id');
+            $table->longText('message_text');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateUserStateTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_state');
+        Schema::dropIfExists('states');
     }
 }
